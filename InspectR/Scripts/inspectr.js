458eba4bea54f60051a3ff51711ecb86bbce9dc9
Acco <Acco@kuilen-online.nl>
2012-11-12 22:48:38 +0100

    Format content automatically when loading requests

--- InspectR/Scripts/inspectr.js
+++ InspectR/Scripts/inspectr.js
@@ -6,7 +6,7 @@
     var InspectR = {
         start: function (config) {
             config = $.extend({}, InspectR.defaults, config);
-            
+
             var viewModel = new InspectRViewModel(config);
             ko.applyBindings(viewModel, config.rootNode);
 
@@ -18,7 +18,7 @@
                 start: viewModel.start,
                 loadSession: viewModel.loadSession
             });
-            
+
             var router = new Router;
 
             $.connection.hub.start({
@@ -40,7 +40,7 @@
 
         // router.on('route:loadSession', self.loadSession);
         // router.on('route:start', self.start);
-        
+
         self.hub = $.connection.inspectRHub;
         var client = self.hub.client;
         var server = self.hub.server;
@@ -48,7 +48,7 @@
         self.UserProfile = ko.observable();
         self.Requests = ko.observableArray([]);
         self.Inspector = ko.observable();
-        
+
         self.RequestList = ko.computed(function () {
             return self.Requests();
         });
@@ -58,11 +58,26 @@
                 .done(function () {
                     server.getUserProfile()
                         .done(self.updateUserProfile);
-                    
+
                     server.getRecentRequests(self.inspectorKey)
                         .done(function (result) {
                             if (result && result.length > 0) {
+
                                 self.Requests(result);
+                                
+                                for (var i = 0; i < result.length; i++) {
+                                    for (var mimeMode in CodeMirror.mimeModes) {
+                                        if (result[i].ContentType === mimeMode) {
+                                            try {
+                                                //CodeMirror throws an error on line regel 3738 in codemirror.js 
+                                                result[i].ContentCodeMirrorMode(mimeMode);
+                                                result[i].ContentCodeMirrorUtil.Format();
+                                            } catch (e) {
+
+                                            }
+                                        }
+                                    }
+                                }
                             }
                         });
                 });
@@ -71,11 +86,11 @@
         self.loadSession = function () {
 
         };
-        
+
         self.updateUserProfile = function (profile) {
             self.UserProfile(profile);
         };
-        
+
         self.clearRecentRequests = function () {
             server.clearRecentRequests(self.inspectorKey);
             self.Requests.removeAll();
@@ -87,7 +102,7 @@
                 + d.getMonth() + '-'
                 + d.getDate();
         };
-        
+
         self.formatTime = function (datestring) {
             var d = new Date(datestring);
             return d.getHours() + ':'
@@ -109,20 +124,29 @@
             };
             return viewmodel;
         };
-        
+
         self.P = function (property, data) {
             if (!ko.isObservable(data[property])) {
                 data[property] = ko.observable();
             }
             return data[property];
         };
-        
+
+
+
         client.requestLogged = function (inspector, request) {
             // console.log('http request');
             // console.log(inspector, request);
             self.Requests.unshift(request);
+
+            if (CodeMirror.mimeModes[request.ContentType]) {
+                
+                request.ContentCodeMirrorMode(request.ContentType);
+                request.ContentCodeMirrorUtil.Format();
+            }
+
         };
     };
 
     $.inspectR = InspectR;
-}(jQuery));
+} (jQuery));
